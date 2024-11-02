16dedbbb563513873ad88654fcd215cf62bc8a2b
aliz1369 <alirezazargarian@gmail.com>
2024-11-02 12:57:02 +0300

    add rotate

--- pdfspliter.py
+++ pdfspliter.py
@@ -11,13 +11,34 @@ def split_pdf(input_pdf_path, output_pdf_path,start_page,end_page):
 
    print(f"PDF pages {start_page} to {end_page} saved as {output_pdf_path}")
       
+def rotate_pdf(input_pdf_path,output_pdf_path,degree):
+   input_pdf = PdfReader(input_pdf_path)
+   output_pdf = PdfWriter()
+   totalPages = len(input_pdf.pages)
 
-input_pdf_path = input("Enter pdf name:")
-output_pdf_path = input("Enter output name:")
-start_page = int(input("Enter Start Page:"))  # Example start page
-end_page = int(input("Enter End Page:"))     # Example end page
+   for i in range(0,totalPages):
+      output_pdf.add_page(input_pdf.pages[i])
+      output_pdf.pages[i].rotate(degree)
 
-split_pdf(input_pdf_path, output_pdf_path, start_page, end_page)
+   with open(output_pdf_path,"wb") as output_file:
+      output_pdf.write(output_file)
+   print(f"PDF rotate with {degree} degree")
+
+
+print(f"1:To split Pdf")   
+print(f"2:To rotate Pdf")   
+input_function_selector = int(input("Enter The Function:"))
+if input_function_selector == 1:
+   input_pdf_path = input("Enter pdf name:")
+   output_pdf_path = input("Enter output name:")
+   start_page = int(input("Enter Start Page:"))  # Example start page
+   end_page = int(input("Enter End Page:"))     # Example end page
+   split_pdf(input_pdf_path, output_pdf_path, start_page, end_page,-90)
+elif input_function_selector == 2:
+   input_pdf_path = input("Enter pdf name:")
+   output_pdf_path = input("Enter output name:")
+   rotate_degree = int(input("Enter rotate degree:"))
+   rotate_pdf(input_pdf_path, output_pdf_path,rotate_degree)
 
 
 
